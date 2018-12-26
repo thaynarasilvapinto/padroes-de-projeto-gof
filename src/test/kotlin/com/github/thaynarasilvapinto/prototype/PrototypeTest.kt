172e7ddb2d1fd76eafc11e05afdd5c3814284177
Thaynara Silva <thay.silva.ptc@gmail.com>
2018-12-26 10:21:29 -0200

    Up Tests

--- src/test/kotlin/com/github/thaynarasilvapinto/prototype/PrototypeTest.kt
+++ src/test/kotlin/com/github/thaynarasilvapinto/prototype/PrototypeTest.kt
@@ -20,5 +20,14 @@ class PrototypeTest {
 		partitura.nota("Re").desenha()
 		partitura.nota("Re").desenha()
 		partitura.nota("Re").desenha()
+		partitura.nota("Do").desenha()
+		partitura.nota("Sol").desenha()
+		partitura.nota("Fa").desenha()
+		partitura.nota("Mi").desenha()
+		partitura.nota("Do").desenha()
+		partitura.nota("Sol").desenha()
+		partitura.nota("La").desenha()
+		partitura.nota("Si").desenha()
+		partitura.nota("Do").desenha()
     }
 }
